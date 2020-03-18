d5a83dc5ea9e72f445ca07825457da8f2bffe0c4
Yash Wardhan Chauhan <yashwardhan75@gmail.com>
2020-03-18 21:24:11 +0530

    second commit

--- first_app/urls.py
+++ first_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('songs/<str:category>',views.songs,name='songs'),
     path('play/<str:song_name>',views.play,name='play'),
-    path('/register',views.register, name='register'),
+    path('register',views.register, name='register'),
     path('login',views.login,name="login"),
     path('logout',views.logout,name="logout")
     
