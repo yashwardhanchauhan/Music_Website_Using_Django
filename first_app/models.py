ba3e63b32562fa6e8c1ce3f0a6580fb9989d6f37
Yash Wardhan Chauhan <yashwardhan75@gmail.com>
2020-05-01 14:04:45 +0530

    updated music file field

--- first_app/models.py
+++ first_app/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Songs(models.Model):
     song_name=models.CharField(max_length=1000)
     choice=(
-      ('Hindi','H'),
-      ('English','E'),
-      ('Punjabi','P'),
+      ('Hindi','Hindi'),
+      ('English','English'),
+      ('Punjabi','Punjabi'),
       )
     category=models.CharField(max_length=100,choices=choice)
     img=models.ImageField(upload_to='pics')
-    audio_file =models.FileField(upload_to='audiofile')
+    audio_file =models.CharField(max_length=1000)
     album=models.CharField(max_length=100)
     artist=models.CharField(max_length=500)
     
